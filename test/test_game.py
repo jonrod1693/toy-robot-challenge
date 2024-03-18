24f257e23ac419516e7accbae9f494b029f9412f
Jon Rodriguez <jonrod1693@gmail.com>
2024-03-18 08:22:32 +0800

    update game tests

--- test/test_game.py
+++ test/test_game.py
@@ -3,63 +3,63 @@ from game.main import Table, Robot, Game
 from unittest import TestCase
 
 class TestGame(TestCase):
-    def test_moves(self):
+    def test_game(self):
         test_table = Table()
         test_robot = Robot()
 
-        # Test Case: test ignore moves before place
-        moves = Game(test_table, test_robot)
-        moves.rotate_left()
-        moves.rotate_right()
-        moves.move()
-        self.assertEqual(moves.report(), None)
+        # Test Case: test ignore commands before place
+        game = Game(test_table, test_robot)
+        game.rotate_left()
+        game.rotate_right()
+        game.move()
+        self.assertEqual(game.report(), None)
 
         # Test Case: test place out of bounds
-        moves.place(-2,-2,"EAST")
-        moves.rotate_left()
-        moves.rotate_right()
-        moves.move()
-        self.assertEqual(moves.report(), None)
+        game.place(-2,-2,"EAST")
+        game.rotate_left()
+        game.rotate_right()
+        game.move()
+        self.assertEqual(game.report(), None)
 
         # Test Case: test exit boundaries at 0,0
-        moves.place(0,0,"SOUTH")
-        moves.move()
-        moves.rotate_right()
-        moves.report()
-        self.assertEqual(moves.report(), "0,0,WEST")
+        game.place(0,0,"SOUTH")
+        game.move()
+        game.rotate_right()
+        game.report()
+        self.assertEqual(game.report(), "0,0,WEST")
 
         # Test Case: test exit boundaries at 4,4
-        moves.place(4,4,"NORTH")
-        moves.move()
-        moves.rotate_right()
-        moves.report()
-        self.assertEqual(moves.report(), "4,4,EAST")
+        game.place(4,4,"NORTH")
+        game.move()
+        game.rotate_right()
+        game.report()
+        self.assertEqual(game.report(), "4,4,EAST")
 
         # Test Case: test traverse from midpoint
-        moves.place(2,2,"NORTH")
-        moves.move()
-        moves.rotate_right()
-        moves.move()
-        moves.rotate_left()
-        moves.move()
-        moves.move()
-        moves.report()
-        self.assertEqual(moves.report(), "3,4,NORTH")
+        game.place(2,2,"NORTH")
+        game.move()
+        game.rotate_right()
+        game.move()
+        game.rotate_left()
+        game.move()
+        game.move()
+        game.report()
+        self.assertEqual(game.report(), "3,4,NORTH")
 
         # Test Case: sample 1 in challenge
-        moves.place(0,0,"NORTH")
-        moves.move()
-        self.assertEqual(moves.report(), "0,1,NORTH")
+        game.place(0,0,"NORTH")
+        game.move()
+        self.assertEqual(game.report(), "0,1,NORTH")
 
         # Test Case: sample 2 in challenge
-        moves.place(0,0,"NORTH")
-        moves.rotate_left()
-        self.assertEqual(moves.report(), "0,0,WEST")
+        game.place(0,0,"NORTH")
+        game.rotate_left()
+        self.assertEqual(game.report(), "0,0,WEST")
 
         # Test Case: sample 3 in challenge
-        moves.place(1,2,"EAST")
-        moves.move()
-        moves.move()
-        moves.rotate_left()
-        moves.move()
-        self.assertEqual(moves.report(), "3,3,NORTH")
+        game.place(1,2,"EAST")
+        game.move()
+        game.move()
+        game.rotate_left()
+        game.move()
+        self.assertEqual(game.report(), "3,3,NORTH")
